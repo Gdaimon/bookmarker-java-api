cd72f26787da9ac47c1c313ad477e0ac954adc20
Alex Arriaga <alex.arriaga.m@gmail.com>
2017-11-20 16:05:04 -0600

    Hacer uso del servicio en el controlador de bookmarks

--- src/zone/webtraining/bookmarker/controllers/BookmarksController.java
+++ src/zone/webtraining/bookmarker/controllers/BookmarksController.java
@@ -6,19 +6,33 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import zone.webtraining.bookmarker.models.Bookmark;
+import zone.webtraining.bookmarker.services.BookmarksService;
+
+import java.util.Collection;
 
 @Controller
 @RequestMapping("/bookmarks")
 public class BookmarksController {
 
+    private BookmarksService bookmarksService;
+
     @GetMapping
-    public ResponseEntity<String> getAll() {
-        return new ResponseEntity<String>("Bookmarks all", HttpStatus.OK);
+    public ResponseEntity<Collection<Bookmark>> getAll() {
+        Collection<Bookmark> bookmarkList = this.bookmarksService.getAll();
+
+        return new ResponseEntity<Collection<Bookmark>>(bookmarkList, HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity<String> create() {
-        return new ResponseEntity<String>("Bookmark created", HttpStatus.CREATED);
+    public ResponseEntity<Bookmark> create() {
+        Bookmark bookmark = this.bookmarksService.get(1);
+
+        return new ResponseEntity<Bookmark>(bookmark, HttpStatus.CREATED);
+    }
+
+    public void setBookmarksService(BookmarksService bookmarksService) {
+        this.bookmarksService = bookmarksService;
     }
 
 }
