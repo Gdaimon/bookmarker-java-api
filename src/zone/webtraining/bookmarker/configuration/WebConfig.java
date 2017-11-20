6df473bd9bfc3775570d252ccd32d1e2c133e81c
Alex Arriaga <alex.arriaga.m@gmail.com>
2017-11-20 15:30:57 -0600

    Corregir mappings en controlador

--- src/zone/webtraining/bookmarker/configuration/WebConfig.java
+++ src/zone/webtraining/bookmarker/configuration/WebConfig.java
@@ -1,18 +1,32 @@
 package zone.webtraining.bookmarker.configuration;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import zone.webtraining.bookmarker.controllers.BookmarksController;
 
 @Configuration
 @EnableWebMvc
+@ComponentScan(basePackages = {"zone.webtraining.bookmarker.controllers"})
 public class WebConfig implements WebMvcConfigurer {
-    @Bean
-    public BookmarksController bookmarksController() {
-        BookmarksController bookmarksController = new BookmarksController();
+//    @Bean
+//    public BookmarksController bookmarksController() {
+//        BookmarksController bookmarksController = new BookmarksController();
+//
+//        return bookmarksController;
+//    }
 
-        return bookmarksController;
-    }
+//    @Bean
+//    public ViewResolver viewResolver() {
+//
+//        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+//        viewResolver.setViewClass(JstlView.class);
+//        viewResolver.setPrefix("/WEB-INF/views");
+//        viewResolver.setSuffix(".jsp");
+//        return viewResolver;
+//    }
+//
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+//    }
 }
--- src/zone/webtraining/bookmarker/controllers/BookmarksController.java
+++ src/zone/webtraining/bookmarker/controllers/BookmarksController.java
@@ -3,14 +3,22 @@ package zone.webtraining.bookmarker.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/bookmarks")
 public class BookmarksController {
 
-    @RequestMapping(name = "bookmarks", method = RequestMethod.GET)
-    public ResponseEntity<String> hello() {
-        return new ResponseEntity<String>("hello", HttpStatus.OK);
+    @GetMapping
+    public ResponseEntity<String> getAll() {
+        return new ResponseEntity<String>("Bookmarks all", HttpStatus.OK);
     }
+
+    @PostMapping
+    public ResponseEntity<String> create() {
+        return new ResponseEntity<String>("Bookmark created", HttpStatus.CREATED);
+    }
+
 }
