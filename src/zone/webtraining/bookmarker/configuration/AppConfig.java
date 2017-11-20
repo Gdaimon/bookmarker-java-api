a58e42519c6b3350eede7a331c816f967cf8ad0d
Alex Arriaga <alex.arriaga.m@gmail.com>
2017-11-20 16:04:33 -0600

    Configurar inyección manual de servicio en controlador

--- src/zone/webtraining/bookmarker/configuration/AppConfig.java
+++ src/zone/webtraining/bookmarker/configuration/AppConfig.java
@@ -1,11 +1,20 @@
 package zone.webtraining.bookmarker.configuration;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import zone.webtraining.bookmarker.services.BookmarksService;
+import zone.webtraining.bookmarker.services.MySQLBookmarksService;
 
 
 @Configuration
 public class AppConfig {
 
+    @Bean
+    @Profile("default")
+    public BookmarksService bookmarksService() {
+        return new MySQLBookmarksService();
+    }
 
 }
--- src/zone/webtraining/bookmarker/configuration/WebConfig.java
+++ src/zone/webtraining/bookmarker/configuration/WebConfig.java
@@ -1,22 +1,42 @@
 package zone.webtraining.bookmarker.configuration;
 
-import org.springframework.context.annotation.ComponentScan;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.StdDateFormat;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
+import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import zone.webtraining.bookmarker.controllers.BookmarksController;
+import zone.webtraining.bookmarker.services.BookmarksService;
+
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = {"zone.webtraining.bookmarker.controllers"})
+//@ComponentScan(basePackages = {"zone.webtraining.bookmarker.controllers"})
 public class WebConfig implements WebMvcConfigurer {
-//    @Bean
-//    public BookmarksController bookmarksController() {
-//        BookmarksController bookmarksController = new BookmarksController();
-//
-//        return bookmarksController;
-//    }
+    @Bean
+    public BookmarksController bookmarksController(BookmarksService bookmarksService) {
+        BookmarksController bookmarksController = new BookmarksController();
 
-//    @Bean
+        bookmarksController.setBookmarksService(bookmarksService);
+        return bookmarksController;
+    }
+
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule( new JodaModule() );
+
+        objectMapper.setDateFormat( new StdDateFormat() );
+
+        converters.add( new MappingJackson2HttpMessageConverter( objectMapper ) );
+    }
+
+    //    @Bean
 //    public ViewResolver viewResolver() {
 //
 //        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
